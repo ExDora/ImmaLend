4a8fdd7c236763bf6efd7aaf2279bcae0798a4ca
ExDora <hernandez.lim2009@gmail.com>
2026-08-31 23:50:21 +0700

    feat: initialize Laravel application structure with routes, testing, and storage configurations

---
+++ app/Http/Controllers/LendingAction/RejectLendingController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\LendingAction;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RejectLendingController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
