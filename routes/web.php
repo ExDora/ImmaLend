60255fcde588d08e25843677c164d118fa31d469
Hernandez Lim <hernandez.lim2009@gmail.com>
2026-09-12 22:12:05 +0700

    Revert "Request page"

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\AdminLoginController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\UserLoginController;
 use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\UserLoginController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\LendingController;
@@ -16,9 +16,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//login user
-//login user
-Route::get('/login', [UserLoginController::class, 'index'])->name('login');
 
 //Login User
 Route::get('/login', [UserLoginController::class, 'create'])->name('login');
@@ -45,14 +42,12 @@ Route::get('/', function () {return view('home');})->name('home');
 Route::resource('items', ItemController::class);
 Route::get('/inventory', function () {return view('items.inventory');})->name('inventory');
 
-    //Request Barang
-    Route::get('/request', [ItemController::class, 'request'])->name('request.item');
-
 
 //Manajemen Data Peminjaman (Resource)
 Route::resource('lendings', LendingController::class);
 Route::get('/admin/borrowing-requests', [LendingController::class, 'adminIndex'])->name('lendings.admin-index');
 Route::get('/admin/return-confirmation', [LendingController::class, 'returnConfirmation'])->name('admin.return-confirmation');
+
 
 //Manajemen Data Aksi Peminjaman (Invokable)
 Route::name('lendings.')->prefix('lendings')->group(function () {
